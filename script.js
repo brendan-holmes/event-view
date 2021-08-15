d6ed2388cf70f80807a248867598274ee9b1a171
Brendan Holmes <brendholmes@gmail.com>
2021-08-15 23:26:12 +1000

    Added more events and mouse trail

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-const minFontSize = 50;
+const minFontSize = 32;
 const maxFontSize = 500;
 const w = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 const h = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
-const content = document.getElementById("content");
+const body = document.getElementsByTagName("body")[0];
 
 const appendNewLine = (stringMessage) => {
-    
-    // new element
     var newElement = document.createElement("span"); 
     newElement.innerText = stringMessage;
     
-    // set style
     newElement.style.position = "fixed";
     newElement.style.left = `${randomInt(0, w)}px`;
     newElement.style.top = `${randomInt(0, h)}px`;
     newElement.style.fontSize = `${randomInt(minFontSize, maxFontSize)}px`;
     newElement.style.color = `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)}`;
-    newElement.className = "visible";
+    newElement.className = "flash";
     
-    // append
-    content.appendChild(newElement);
+    body.appendChild(newElement);
 }
 
 const randomInt = (min, max) => {
     return Math.round(Math.random() * (max - min) + min)
 }
 
-const handleKeyboardEvent = (keyboardEvent) => {
-    const key = keyboardEvent.key;
-    const message = `${key}`;
-    appendNewLine(message);
-};
+window.addEventListener("keydown", (event) => appendNewLine(`${event.key}`));
+window.addEventListener("click", () => appendNewLine(`click`));
+window.addEventListener("dblclick", () => appendNewLine(`double click`));
+window.addEventListener("touchstart", () => appendNewLine(`touch`));
+window.addEventListener("touchmove", () => appendNewLine(`touch move`));
+window.addEventListener("focus", () => appendNewLine(`window focus`));
 
-window.addEventListener("keydown", handleKeyboardEvent);
+document.addEventListener('mousemove', e => {
+    var newElement = document.createElement("span"); 
+
+    newElement.style.left = e.pageX + 'px';
+    newElement.style.top = e.pageY + 'px';
+    newElement.className = "mouse-object"
+    newElement.style.backgroundColor = `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)}`;
+
+    body.appendChild(newElement);
+});
